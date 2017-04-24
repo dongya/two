9c78df577e56fcad64bf1f7acfaa989c3c8b6632
dongya <635852868@qq.com>
2017-04-24 16:14:33 +0800

    更改文件

---
+++ MyAndroid/app/src/main/java/com/example/edianzu/myandroid/TwoActivity.java
@@ -0,0 +1,15 @@
+package com.example.edianzu.myandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by edianzu on 2017/4/24.
+ */
+
+public class TwoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
