c18af341110f8a5bcca16806cf54e15425495d4c
dongya <635852868@qq.com>
2017-06-02 11:09:39 +0800

    分支类容修改

--- MyAndroid/app/src/main/java/com/example/edianzu/myandroid/MainActivity.java
+++ MyAndroid/app/src/main/java/com/example/edianzu/myandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.edianzu.myandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Log.d("dongya","dongya");
             }
         });
     }
