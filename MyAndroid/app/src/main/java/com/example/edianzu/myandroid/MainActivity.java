f04e56cb224e141d7e5576a66f63877c0f7a7849
dongya <635852868@qq.com>
2017-06-02 11:07:45 +0800

    分支提交修改

--- MyAndroid/app/src/main/java/com/example/edianzu/myandroid/MainActivity.java
+++ MyAndroid/app/src/main/java/com/example/edianzu/myandroid/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.edianzu.myandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
